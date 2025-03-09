48fd11263f58940c74297d8635ffe6383b7611e2
OrlayMolina <111409267+OrlayMolina@users.noreply.github.com>
2025-03-08 21:42:57 -0500

    Se agrga el controller para el servicio de authetication.

---
+++ src/main/java/co/edu/uniquindio/ing/soft/pasteleria/application/ports/input/ManageAuthUseCase.java
@@ -0,0 +1,9 @@
+package co.edu.uniquindio.ing.soft.pasteleria.application.ports.input;
+
+import co.edu.uniquindio.ing.soft.pasteleria.application.dto.LoginDTO;
+import co.edu.uniquindio.ing.soft.pasteleria.application.dto.TokenDTO;
+import co.edu.uniquindio.ing.soft.pasteleria.domain.exception.DomainException;
+
+public interface ManageAuthUseCase {
+    TokenDTO logIn(LoginDTO loginDTO) throws DomainException;
+}
